5d2e37122e107de476f558e3211e389b6e39ad43
Joonas Mankki <joonas@avocado.fi>
2016-03-10 21:19:31 +0200

    fixed homepage on main js-file

--- jquery.photostrip.js
+++ jquery.photostrip.js
@@ -7,7 +7,7 @@
  *   http://www.opensource.org/licenses/mit-license.php
  *
  * Project home:
- *   http://www.github.net/jiinus/jquery.photostrip.js
+ *   http://www.github.net/jiinus/photostrip
  *
  * Version:  1.0.0
  */
